0667eee5c0206a74844ca7b343e51f84e77b7baf
MarjaCarty <marja.d.carty@gmail.com>
2020-10-20 12:44:44 -0500

    Form styling

--- food-truck-trackr/src/components/AddTruckForm.js
+++ food-truck-trackr/src/components/AddTruckForm.js
@@ -1,4 +1,42 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+//////styles////////
+const StyledTruckForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    font-size: 3rem;
+    margin: 3%;
+  }
+
+  label {
+    margin-bottom: 1%;
+  }
+
+  input[type="text"] {
+    display: block;
+    margin: 4%;
+    border: none;
+    background-color: ${(props) => props.theme.paleYellow};
+    padding: 6%;
+    border-bottom: 2px solid ${(props) => props.theme.grey};
+  }
+
+  button {
+    padding: 0.5% 4%;
+    background-color: ${(props) => props.theme.button};
+    color: ${(props) => props.theme.grey};
+    border: none;
+    margin: 2%;
+  }
+
+  img {
+    margin: 3% 0;
+  }
+`;
 
 ////////////initial values/////////////
 const initialFormValues = {
@@ -37,7 +75,7 @@ export default function AddTruckForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <StyledTruckForm onSubmit={handleSubmit}>
       <h2>Add A Truck!</h2>
 
       <label>
@@ -86,6 +124,6 @@ export default function AddTruckForm() {
       })}
 
       <button type="submit">Submit</button>
-    </form>
+    </StyledTruckForm>
   );
 }
